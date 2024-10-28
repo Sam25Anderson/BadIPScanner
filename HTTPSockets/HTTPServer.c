8fc0eac9fe0829c61a6ec435dac259eb2bcbe8aa
Sam25Anderson <sameulsanderson328@gmail.com>
2024-10-28 11:43:15 -0500

    Adding notes to code

--- HTTPSockets/HTTPServer.c
+++ HTTPSockets/HTTPServer.c
@@ -1,5 +1,3 @@
-// Server side C program to demonstrate Socket
-// programming
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,32 +16,42 @@ int main(int argc, char const* argv[])
     char* hello = "Hello from server";
 
     // Creating socket file descriptor
+    // socket arg1: domain (IPv4 or IPv6), arg2: type (TCP or UDP), arg3: protocol (0 is a default TCP protocol)
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     // Forcefully attaching socket to the port 8080
+    // setsockopt arg1: socket (created in previous if statement), arg2: level (SOL_SOCKET is socket level),
+    // arg3: option name (rules of binding), arg4: opt value (ours is a point to a set value), arg5: option length
     if (setsockopt(server_fd, SOL_SOCKET,
                    SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+
+    // specifies that the socket will communicate with IPv4
     address.sin_family = AF_INET;
+    // tells socket to listen on all available network interfaces
     address.sin_addr.s_addr = INADDR_ANY;
+    // sets port number for socket
     address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port 8080
+    // bind arg 1: socket, arg2: refers to sockaddr struct info, arg3: address size
     if (bind(server_fd, (struct sockaddr*)&address,
              sizeof(address))
         < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
+    // listen arg1: socket, arg2: backlog
     }
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
+    // accept arg1: socket, arg2: refers to sockaddr stuct info, arg3: address length
     }
     if ((new_socket
          = accept(server_fd, (struct sockaddr*)&address,
